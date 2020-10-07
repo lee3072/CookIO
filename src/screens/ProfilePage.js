cd2e7445d02112722d15e54db445aad324d6c49e
Jihyun Pyun <pyunj30@gmail.com>
2020-10-06 21:17:48 -0400

    Firebase and frondend set
    Firebase and frondend set.
    Need to firgure out re-rendering the page.

--- src/screens/ProfilePage.js
+++ src/screens/ProfilePage.js
@@ -1,12 +1,13 @@
 import React from 'react';
 //import React, { useState, useEffect } from 'react';
-import { StyleSheet, Button, Text, View } from 'react-native';
+import { StyleSheet, Button, Image, Text, View } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import firebase from '../../firebase_setup';
 import 'firebase/firestore';
+//import styles from '../styles/auth_styles';
 
 var db = firebase.firestore(); //firestore
 
@@ -20,6 +21,7 @@ class ProfilePage extends React.Component {
             username: '',
             followers: 0,
             interest: '',
+            icon: '',
 
         };
     }
@@ -69,42 +71,132 @@ class ProfilePage extends React.Component {
             .then(topicOfInterest => {
                 this.setState({interest: topicOfInterest})
             });
-    }
+
+            function getIcon(documentSnapshot) {
+                return documentSnapshot.get('userIcon');
+            }
+              
+            db.collection('Users')
+            .doc(currentUser.uid)
+            .get()
+            .then(documentSnapshot => getIcon(documentSnapshot))
+            .then(userIcon => {
+                this.setState({icon: userIcon})
+            });
+        }
 
     }
 
     render() {
         return (
-            <View
-                style={{
-                lex: 1,
-                justifyContent: "center",
-                alignItems: "center"
-            }}>
-            <Text>Username: {this.state.username}</Text>
-            <Text>Number of Followers: {this.state.followers}</Text>
-            <Text>Topic of Interest: {this.state.interest}</Text>
-            <Text>Welcome!!</Text>
+            <View style={styles.container}>
+                <View style={styles.logoutButton}>
+                    <Button
+                        color= "#ffdb85"
+                        title="Logout"
+                        onPress={() => {
+                                firebase.auth().signOut()
+                                this.props.navigation.navigate('SignInPage')
+                            }}
+                    />
+                </View>
+                <View style={{ margineTop: 60}}>
+                    <View style = {styles.profileContainer}>
+                        
+                        <Image
+                            style={styles.icon}
+                            source={
+                                this.state.icon
+                                    ? { uri: this.state.icon }
+                                    : require("../assets/temp_icon.jpg")}/>
+                        <Text style={styles.username}>{this.state.username}</Text>
+                    </View>
+                    <View style={styles.interestBox}>
+                        <Text style={styles.interestedIn}>Interested in..</Text>
+                        <Text style={styles.interest}>{this.state.interest}</Text>
+                    </View>
+                </View>
 
-            <Button
-                title="Change Profile"
-                onPress={() => this.props.navigation.navigate('EditProfilePage')}
-            />
-            <Button
-                title="Make Post"
-                onPress={() => this.props.navigation.navigate('MakePostPage')}
-            />
-            <Button
-                title="Logout"
-                onPress={() => {
-                        firebase.auth().signOut()
-                        this.props.navigation.navigate('SignInPage')
-                    }}
-            />
+                <View style={styles.rowContainer}>
+                    <View style={styles.followerFollowing}>
+                        <Text>Follower</Text>
+                        <Text>{this.state.followers}</Text>
+                    </View>
+                    <View style={styles.followerFollowing}>
+                        <Text>Followings</Text>
+                        <Text>N/A</Text>
+                    </View>
+                    <Button color= "#ffb300"
+                        title="Change Profile"
+                        onPress={() => this.props.navigation.navigate('EditProfilePage')}
+                    />
+                </View>
+                
+                <View style={styles.buttonMiddle}>
+                    <Button color= "#ffb300"
+                        title="Make Post"
+                        onPress={() => this.props.navigation.navigate('MakePostPage')}
+                    />
+                </View>
+                
+
+            
             </View>
         );
     }
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        //alignItems: "center",
+        padding: 13,
+    },
+    profileContainer: {
+        alignItems: "center",
+        marginTop: 10,
+    }, 
+    icon: {
+        width: 146,
+        height: 146,
+        borderRadius: 68,
+    },
+    username: {
+        fontSize: 30,
+        //color: "#363636",
+        fontWeight: "bold",
+        paddingVertical: 10,
+    },
+    interestBox: {
+        alignItems: "center",
+    },
+    interest: {
+        fontSize: 15,
+        color: "#636363",
+        //marginLeft: 15,
+    },
+    interestedIn: {
+        fontSize: 18,
+        color: "#636363",
+    },
+    rowContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+    },
+    buttonMiddle: {
+        paddingHorizontal: 10,
+    },
+    followerFollowing: {
+        padding: 20,
+        alignItems: "center",
+    },
+    logoutButton: {
+        paddingTop: 20,
+        flexDirection: "row",
+        justifyContent: "flex-end",
+    },
+
+});
 
 export default ProfilePage;
